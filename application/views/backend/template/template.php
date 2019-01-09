b54c763637b9dd4b1de4afd7359ffdeac0e7d9bf
gabrielcaetanodm <gabrielcaetanodm@gmail.com>
2019-01-09 14:46:47 -0200

    template backend

---
+++ application/views/backend/template/template.php
@@ -0,0 +1,60 @@
+<div id="wrapper">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Navegação</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html">Painel Administrativo</a>
+        </div>
+        <!-- /.navbar-top-links -->
+
+        <div class="navbar-default sidebar" role="navigation">
+            <div class="sidebar-nav navbar-collapse">
+                <ul class="nav" id="side-menu">
+                    <li>
+                        <a href="<?php echo base_url('admin/slider') ?>"><i class="fa fa-slideshare fa-fw"></i> Slider</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url('admin/postagens') ?>"><i class="fa fa-edit fa-fw"></i> Postagens</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url('admin/fotos') ?>"><i class="fa fa-camera-retro fa-fw"></i> Fotos</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url('admin/galeria') ?>"><i class="fa fa-image fa-fw"></i> Galeria</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url('admin/projetos') ?>"><i class="fa fa-trophy fa-fw"></i> Projetos</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url('admin/servicos') ?>"><i class="fa fa-clipboard fa-fw"></i> Serviços</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url('admin/sobre_nos') ?>"><i class="fa fa-pencil fa-fw"></i> Sobre Nós</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url('admin/processo_seletivo') ?>"><i class="fa fa-coffee fa-fw"></i> Processo Seletivo</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url('admin/cliente_op') ?>"><i class="fa fa-thumbs-o-up fa-fw"></i> Opniões de Clientes</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url('admin/contato') ?>"><i class="fa fa-at fa-fw"></i> Contato</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url('admin/usuarios') ?>"><i class="fa fa-wrench fa-fw"></i> Usuários</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url('admin/usuarios/logout') ?>"><i class="fa fa-sign-out fa-fw"></i> Sair do Sistema</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.sidebar-collapse -->
+        </div>
+        <!-- /.navbar-static-side -->
+    </nav>
