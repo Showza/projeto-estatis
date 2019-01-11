53f2342ab7ddc7fc72647f6ba61173e290d7be3e
SabrinaSuellengggijklasdfasdf <sabrinasuellen1@gmail.com>
2019-01-10 22:22:54 -0200

    navbar

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,13 +1,13 @@
 <div class="navbar">
   <div class="logo"> 
-    <a href="#"><img src="assets/img/LOGO.png"></a>
+    <a href="#"><img src="assets/frontend/img/LOGO.png"></a>
   </div>
   <div class="menu">
       <a class="active" href="#">INICIO</a>  
       <a href="#">QUEM SOMOS</a>
-			<a href="#">SERVIÇOS</a>
-			<a href="#">BLOG</a>
+      <a href="#">SERVIÇOS</a>
+      <a href="#">BLOG</a>
       <a href="#">CONTATO</a>
-			
+      
       </div>
 </div>
