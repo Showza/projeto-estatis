4d38f2a99d6e426cf7d9ae4283a10477e0f5541e
SabrinaSuellengggijklasdfasdf <sabrinasuellen1@gmail.com>
2019-01-18 13:16:56 -0200

    correção

--- application/views/frontend/template/header.php
+++ application/views/frontend/template/header.php
@@ -1,25 +1,6 @@
-<header class="menu">
-  <a href="#"><img class="logo" src="<?php echo base_url('/assets/frontend/img/logo.png'); ?>"></a>  
-  <nav class="navli">
-    <li><a href="#">INICIO</a></li>
-    <li><a href="#">QUEM SOMOS</a></li>
-    <li><a href="#">SERVIÇOS</a></li>
-    <li><a href="#">BLOG</a></li>
-    <li><a href="#">CONTATO</a></li>
-  </nav>
-</header>
-
-
-
-<!--
 <div class="navbar">
-<<<<<<< HEAD
-  <div class="logo"> 
-    <a href="#"><img src=""></a>
-=======
   <div class="logo">
     <a href="#"><img src="<?php echo base_url('/assets/frontend/img/logo.png'); ?>"></a>
->>>>>>> f03ba2cf7b9b83d2e4b18715680ea6f7f05e9558
   </div>
   <div class="menu">
       <a href="#home">INICIO</a>
@@ -29,7 +10,3 @@
       <a href="#contato">CONTATO</a>
       </div>
 </div>
-<<<<<<< HEAD
--->
-=======
->>>>>>> f03ba2cf7b9b83d2e4b18715680ea6f7f05e9558
