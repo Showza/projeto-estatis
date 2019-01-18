50948f6bbe1e62add78c5c370df8f34b621c9d46
SabrinaSuellengggijklasdfasdf <sabrinasuellen1@gmail.com>
2019-01-17 23:10:51 -0200

    desgraça

---
+++ application/views/frontend/template/header.php
@@ -0,0 +1,35 @@
+<header class="menu">
+  <a href="#"><img class="logo" src="<?php echo base_url('/assets/frontend/img/logo.png'); ?>"></a>  
+  <nav class="navli">
+    <li><a href="#">INICIO</a></li>
+    <li><a href="#">QUEM SOMOS</a></li>
+    <li><a href="#">SERVIÇOS</a></li>
+    <li><a href="#">BLOG</a></li>
+    <li><a href="#">CONTATO</a></li>
+  </nav>
+</header>
+
+
+
+<!--
+<div class="navbar">
+<<<<<<< HEAD
+  <div class="logo"> 
+    <a href="#"><img src=""></a>
+=======
+  <div class="logo">
+    <a href="#"><img src="<?php echo base_url('/assets/frontend/img/logo.png'); ?>"></a>
+>>>>>>> f03ba2cf7b9b83d2e4b18715680ea6f7f05e9558
+  </div>
+  <div class="menu">
+      <a href="#">INICIO</a>  
+      <a href="#">QUEM SOMOS</a>
+      <a href="#">SERVIÇOS</a>
+      <a href="#">BLOG</a>
+      <a href="#">CONTATO</a>
+      </div>
+</div>
+<<<<<<< HEAD
+-->
+=======
+>>>>>>> f03ba2cf7b9b83d2e4b18715680ea6f7f05e9558
