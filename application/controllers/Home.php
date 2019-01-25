24233af3ce018f8d06925ec6681750b225dcbff9
rogerd ribeiro <rogerdribeiro@gmail.com>
2019-01-25 18:07:42 -0200

    backend pagina de depoimentos [CRUD]

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,11 +6,14 @@
             parent::__construct();
 
             $this->load->model('slider_model', 'modelslider');
+            $this->load->model('depoimentos_model','modeldepoimentos');
         }
 
         public function index()
         {
             $dados['slider'] = $this->modelslider->mostrar_slides();
+
+            $dados['listadepoimentos'] = $this->modeldepoimentos->listar_depoimentos();
 
             //Dados a serem enviados para o Cabeçalho
 		    $dados['titulo'] = 'Estatis Jr.';
@@ -18,7 +21,7 @@
 
             $this->load->view('frontend/template/html-header', $dados);
             $this->load->view('frontend/template/header');
-            $this->load->view('frontend/depoimentos');
+            $this->load->view('frontend/home');
             $this->load->view('frontend/template/footer');
             $this->load->view('frontend/template/html-footer');
         }
