9afbfc24ecca813ec6e0f81ed1921956b6bd6370
rogerd ribeiro <rogerdribeiro@gmail.com>
2019-01-31 14:42:52 -0200

    alterações depoimentos

--- application/views/frontend/depoimentos.php
+++ application/views/frontend/depoimentos.php
@@ -3,8 +3,8 @@
 <div id="depoimentos">
     <h2 class="subtitle">OPINIÃO DOS NOSSOS CLIENTES</h2>
     <?php $i=0; foreach($listadepoimentos as $depoimentos) {?>
-            <div class="content-depo"  style="height:500px;width:100%;border:1px solid #ccc;font:16px/26px Georgia, Garamond, Serif;overflow:auto;" >
-                <div class="depo1">
+            <div class="content-depo" >
+                <div class="depo1" style="width: 70%">
                     <p class="depo-txt"><?php echo $listadepoimentos[$i]->descricao?> </p>
                     <div class="author-depo">
                         <h4> <strong><?php echo $listadepoimentos[$i]->autor?>  </strong></h4>
