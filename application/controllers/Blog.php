45ec6d6523ee82c0bb9360ac6933ce1752a0c4fd
gabrielcaetanodm <gabrielcaetanodm@gmail.com>
2019-02-06 18:56:55 -0200

    frontend blog, att da navbar e bd

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -6,8 +6,22 @@
             parent::__construct();
         }
 
-        public function index()
+        public function index($pular=null, $post_por_pagina=null)
         {
+
+            $this->load->library('pagination');
+            $this->load->model('blog_model', 'modelblog');
+
+            $config['base_url'] = base_url("blog/");
+    		$config['total_rows'] = $this->modelblog->contar();
+    		$post_por_pagina = 4;
+    		$config['per_page'] = $post_por_pagina;
+
+    		$this->pagination->initialize($config);
+
+    		$dados['links_paginacao'] = $this->pagination->create_links();
+
+    		$dados['postagem'] = $this->modelblog->mostrar_publicacao($pular, $post_por_pagina);
 
             //Dados a serem enviados para o Cabeçalho
 		    $dados['titulo'] = 'Estatis Jr.';
