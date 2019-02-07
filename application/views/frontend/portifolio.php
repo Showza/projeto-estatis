f178e2ce3371fb188e1f67b6ac0bd329076e22f8
rogerd ribeiro <rogerdribeiro@gmail.com>
2019-02-07 14:52:12 -0200

    portifolio frontend

--- application/views/frontend/portifolio.php
+++ application/views/frontend/portifolio.php
@@ -2,61 +2,28 @@
     <h2 class="subtitle"><strong>NOSSOS PROJETOS</strong></h2>
 
     <div class="portifolio-container">
+        
         <div class="portifolio-content">
-             <a href="https://img1.ibxk.com.br/2017/07/13/13160112901226.jpg?w=700" target="_blank">
+            <div target="_blank">
             <img src="https://img1.ibxk.com.br/2017/07/13/13160112901226.jpg?w=700" alt="Fjords" style="width:100%">
             <div class="caption">
+                <h4><strong>Titulo</strong></h4>
                 <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
             </div>
-            </a>   
+            </div>   
         </div>
 
         <div class="portifolio-content">
-            <a href="https://img1.ibxk.com.br/2017/07/13/13160112901226.jpg?w=700" target="_blank">
+            <div target="_blank">
             <img src="https://img1.ibxk.com.br/2017/07/13/13160112901226.jpg?w=700" alt="Fjords" style="width:100%">
             <div class="caption">
+                <h4><strong>Titulo</strong></h4>
                 <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
             </div>
-            </a>    
+            </div>   
         </div>
         
-        <div class="portifolio-content">
-            <a href="https://img1.ibxk.com.br/2017/07/13/13160112901226.jpg?w=700" target="_blank">
-            <img src="https://img1.ibxk.com.br/2017/07/13/13160112901226.jpg?w=700" alt="Fjords" style="width:100%">
-            <div class="caption">
-                <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
-            </div>
-            </a>
-        </div>
-        
-        <div class="portifolio-content">
-            <a href="https://img1.ibxk.com.br/2017/07/13/13160112901226.jpg?w=700" target="_blank">
-            <img src="https://img1.ibxk.com.br/2017/07/13/13160112901226.jpg?w=700" alt="Fjords" style="width:100%">
-            <div class="caption">
-                <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
-            </div>
-            </a>
-        </div>
 
-        <div class="portifolio-content">
-            <a href="https://img1.ibxk.com.br/2017/07/13/13160112901226.jpg?w=700" target="_blank">
-            <img src="https://img1.ibxk.com.br/2017/07/13/13160112901226.jpg?w=700" alt="Fjords" style="width:100%">
-            <div class="caption">
-                <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
-            </div>
-            </a>
-        </div>
-        
-        <div class="portifolio-content">
-             <a href="https://img1.ibxk.com.br/2017/07/13/13160112901226.jpg?w=700" target="_blank">
-            <img src="https://img1.ibxk.com.br/2017/07/13/13160112901226.jpg?w=700" alt="Fjords" style="width:100%">
-            <div class="caption">
-                <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
-            </div>
-            </a>
-        </div>
-       
-                        
 
     </div>
 </div>
